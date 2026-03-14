bccef1af7c80046725642528c4176d7b7b2c1ede
Marco Pollacci <pollacci@gmail.com>
2026-03-14 12:33:30 +0100

    build(project): ⬆️ upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,66 +4,64 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.local(),
-        name: "Graphik Semibold",
-        cssVariable: "--talk-marco-graphik-semibold",
-        options: {
-          variants: [
-            {
-              src: ["./src/fonts/GraphikSemibold.otf"],
-              weight: 700,
-              style: "normal",
-            },
-          ],
-        },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Graphik Semibold",
+      cssVariable: "--talk-marco-graphik-semibold",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/GraphikSemibold.otf"],
+            weight: 700,
+            style: "normal",
+          },
+        ],
       },
-      {
-        provider: fontProviders.local(),
-        name: "Graphik Regular",
-        cssVariable: "--talk-marco-graphik-regular",
-        options: {
-          variants: [
-            {
-              src: ["./src/fonts/GraphikRegular.otf"],
-              weight: 400,
-              style: "normal",
-            },
-          ],
-        },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Graphik Regular",
+      cssVariable: "--talk-marco-graphik-regular",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/GraphikRegular.otf"],
+            weight: 400,
+            style: "normal",
+          },
+        ],
       },
-      {
-        provider: fontProviders.local(),
-        name: "Graphik Medium",
-        cssVariable: "--talk-marco-graphik-medium",
-        options: {
-          variants: [
-            {
-              src: ["./src/fonts/GraphikMedium.otf"],
-              weight: 500,
-              style: "normal",
-            },
-          ],
-        },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Graphik Medium",
+      cssVariable: "--talk-marco-graphik-medium",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/GraphikMedium.otf"],
+            weight: 500,
+            style: "normal",
+          },
+        ],
       },
-      {
-        provider: fontProviders.local(),
-        name: "Audio Wide Regular",
-        cssVariable: "--talk-marco-audio-wide-regular",
-        options: {
-          variants: [
-            {
-              src: ["./src/fonts/Audiowide-Regular.ttf"],
-              weight: 400,
-              style: "normal",
-            },
-          ],
-        },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Audio Wide Regular",
+      cssVariable: "--talk-marco-audio-wide-regular",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/Audiowide-Regular.ttf"],
+            weight: 400,
+            style: "normal",
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
   vite: {
     build: {
       rollupOptions: {
--- src/js/deck.js
+++ src/js/deck.js
@@ -7,5 +7,7 @@ styleHeader.innerHTML = `header { display: flex;
   }
  }`;
 
-const headerSpeackerAside = document.querySelector("p-deck");
-headerSpeackerAside.shadowRoot.appendChild(styleHeader);
+window.onload = () => {
+  const headerSpeackerAside = document.querySelector("p-deck");
+  headerSpeackerAside.shadowRoot.appendChild(styleHeader);
+};
